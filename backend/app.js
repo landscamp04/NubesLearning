a9d9175675a9debb33fac6e4e209ba093a1a7afa
landscamp04 <007960622@coyote.csusb.edu>
2025-06-06 02:31:45 -0700

    IAM Roles for IRSA (Deployment Ready), Initial Public push for CI/CD to ECR

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const AWS = require("aws-sdk"); // AWS SDK for JavaScript
+
 const app = express();
 const PORT = 5000;
 
-app.get('/api/hello', (req, res) => {
-    res.json({message: "Hello Nubes! Youre in the Backend now."});
+app.get('/api/hello', async (req, res) => {
+    const s3 = new AWS.S3();
+
+    try {
+        //below is some code for production S3 bucket listing. It fails in my local minikube cluster, but will work in AWS EKS deployment
+        const data = await s3.listBuckets().promise();
+        const bucketNames = data.Buckets.map(bucket => bucket.Name);
+        res.json({ message: "Hello Nubes! You're in the Backend now.", buckets: bucketNames });
+    } catch (err) {
+        if (err.code === 'CredentialsError' || err.code === 'UnknownEndpoint') {
+            res.json({ message: "Hello Nubes! You're in the Backend now. (No AWS credentials found)" });
+        } else {
+            res.json({ message: `Hello Nubes! AWS error: ${err.message}` });
+        }
+    }
 });
 
 app.listen(PORT, () => console.log(`backend is running on port ${PORT}`));
